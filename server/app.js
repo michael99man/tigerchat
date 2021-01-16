1e96e0db4997ac08435ad2b481ca69e15d4b68d3
Michael Man <17michaelm@gmail.com>
2021-01-16 03:53:41 -0800

    Added matching engine code

--- server/app.js
+++ server/app.js
@@ -37,52 +37,136 @@ var ROOM_ID_COUNTER = 0
 // map of room_id -> message list
 const messages = new Map()
 
-// mapping of each connected netid to the associated socket and room
+// mapping of netid -> {socket, room_id}
 const connections = new Map()
 
 // mapping of room_id -> pair of users
 const rooms = new Map()
 
 // queue of people waiting to be connected
-var in_search = []
+// mapping of netid -> {netid, match_mode, profile}
+var match_queue = new Map()
 
 /*
  * Core business logic
  */
 
+// triggered when client requests a match
 const onFindMatch = (io, socket, match_mode) => {
     var netid = auth.getNetid(socket.request)
     console.log(`Init match from ${netid} in mode ${match_mode}`);
 
-    // TODO: use match_mode value
-    in_search.push(netid)
-    checkForMatch(io)
+    // if already in queue, remove
+    if (match_queue.has(netid)) {
+        console.log(`Dequeued match profile ${JSON.stringify(match_queue.get(netid))}`)
+        match_queue.delete(netid)
+    }
+        
+    // represents user and what they are matching for 
+    var matchProfile = {
+        netid: netid,
+        match_mode: match_mode,
+        profile: getStudent(netid)
+    }
+
+    if (matchProfile.profile == null) {
+        console.error(`${netid} tried to match in mode ${match_mode} with empty profile. Setting to [ANYONE]`)
+        matchProfile.match_mode = MATCH_MODE.ANYONE
+    }
+
+    // on a new init match, scan through the current queue and check for match criteria
+    // linear scan, probably fine
+    
+    const keys = Array.from( match_queue.keys() );
+    for (var i = 0; i < keys.length; i++){
+        const key = keys[i]
+        const mp_b = match_queue.get(key)
+
+        if (isMatch(matchProfile, mp_b)) {
+            // remove from queue
+            match_queue.delete(key)
+            console.log(`Matching profiles ${JSON.stringify(matchProfile)} & ${JSON.stringify(mp_b)}`)
+            matchUsers(io, netid, mp_b.netid)
+            return
+        }
+    }
+     
+    // no match found
+    console.log(`Put match profile into queue ${JSON.stringify(matchProfile)}`)
+    match_queue.set(netid, matchProfile)
 }
 
-function checkForMatch(io) {
-    if (in_search.length >= 2) {
-        // pop the first two and hook them up
-        var userA = in_search.shift();
-        var userB = in_search.shift();
+// checks two matchProfiles and returns true iff criteria is met for both people
+function isMatch(mp_a, mp_b) {
+    // return true IFF a accepts b (vice versa is not guaranteed)
+    function oneWayMatch(_mp_a, _mp_b) {
+        if (_mp_a.match_mode === MATCH_MODE.ANYONE) {
+            return true;
+        }
+        // invariant: profile will be defined for _mp_a at this point
+        if (_mp_b.profile == null) {
+            return false;
+        }
 
-        // tell each other that they've matched
-        dataA = connections.get(userA)
-        dataB = connections.get(userB)
-
-        room_id = ROOM_ID_COUNTER
-        dataA.socket.join(room_id)
-        dataB.socket.join(room_id)
-        dataA.room_id = room_id
-        dataB.room_id = room_id
-
-        messages.set(room_id, [])
-
-        io.to(room_id).emit("match")
-        rooms.set(room_id, { userA: userA, userB: userB })
-        ROOM_ID_COUNTER += 1
-
-        console.log(`Matched users ${userA} and ${userB}, sent to room ${room_id}`)
+        switch (_mp_a.match_mode) {
+            case MATCH_MODE.CLASS:
+                return _mp_a.profile['class_year'] === _mp_b.profile['class_year']
+            case MATCH_MODE.MAJOR:
+                return _mp_a.profile['major_code'] === _mp_b.profile['major_code']
+            case MATCH_MODE.RES_COLLEGE:
+                return _mp_a.profile['res_college'] === _mp_b.profile['res_college']
+            default:
+                console.log(`Invalid match_mode: ${_mp_a.MATCH_MODE}`)
+                return false
+        }
     }
+
+    // match if both match with each other
+    if (oneWayMatch(mp_a, mp_b) && oneWayMatch(mp_b, mp_a)) {
+        return true;
+    }
+    return false;
+}
+
+// matches two users together and creates a new room
+function matchUsers(io, netid_a, netid_b) {
+    // tell each other that they've matched
+    data_a = connections.get(netid_a)
+    data_b = connections.get(netid_b)
+
+    // check invariants
+    if (data_a === undefined) {
+        console.error(`No connection found for ${netid_a}`)
+        return;
+    }
+    if (data_b === undefined) {
+        console.error(`No connection found for ${netid_b}`)
+        return;
+    }
+    if (data_a.room_id != -1) {
+        console.error(`${netid_a} is already in room ${data_a.room_id}`)
+        return;
+    }
+    if (data_b.room_id != -1) {
+        console.error(`${netid_a} is already in room ${data_b.room_id}`)
+        return;
+    }
+
+    // TODO: make a better room counter
+    room_id = ROOM_ID_COUNTER
+    
+    data_a.socket.join(room_id)
+    data_b.socket.join(room_id)
+    data_a.room_id = room_id
+    data_b.room_id = room_id
+
+    messages.set(room_id, [])
+
+    io.to(room_id).emit("match")
+    rooms.set(room_id, { netid_a: netid_a, netid_b: netid_b })
+    ROOM_ID_COUNTER += 1
+
+    console.log(`Matched users ${netid_a} and ${netid_b}, sent to room ${room_id}`)
 }
 
 // returns whether or not to terminate connection
@@ -114,10 +198,11 @@ const onSocketConnect = (io, socket, netid) => {
 const onSocketDisconnect = (io, socket, netid) => {
     var netid = auth.getNetid(socket.request)
     // remove user if they are queued to match
-    in_search = in_search.filter(user => user !== netid);
+    if (match_queue.has(netid)) {
+        match_queue.delete(netid)
+    }
 
-    console.log(connections.get(netid))
-    // if user is in room, broadcast that they have disconnected
+    // if user is in room, broadcast to partner that they have disconnected
     var room_id = connections.get(netid).room_id
     if (room_id !== -1) {
         console.log(`Broadcasting to room ${room_id} that ${netid} has disconnected`)
